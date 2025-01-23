a93ab574d623df0782a13bbf0e295f27b123ace1
Frytura-dev <fryrura@gmail.com>
2025-01-23 13:34:19 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/BirdSanctuary",
+  base:"/BirdSanctuary/",
   build: {
     assetsDir: "assets",
     sourcemap: true,
